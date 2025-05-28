0def43a3473243a61dda5eb9c6f1a6d73a04b2c8
r4dbz <108780431+r4db@users.noreply.github.com>
2025-05-28 22:57:53 +0800

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -6,9 +6,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/omni_Infinix-X6515.mk
+    $(LOCAL_DIR)/twrp_Infinix-X6515.mk
 
 COMMON_LUNCH_CHOICES := \
-    omni_Infinix-X6515-user \
-    omni_Infinix-X6515-userdebug \
-    omni_Infinix-X6515-eng
+    twrp_Infinix-X6515-user \
+    twrp_Infinix-X6515-userdebug \
+    twrp_Infinix-X6515-eng
