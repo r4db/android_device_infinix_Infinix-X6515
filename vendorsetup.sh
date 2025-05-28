f371978a170762f5c15ab36d02bb4874eeab809b
r4dbz <108780431+r4db@users.noreply.github.com>
2025-05-28 17:51:31 +0800

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-add_lunch_combo omni_Infinix-X6515-user
-add_lunch_combo omni_Infinix-X6515-userdebug
-add_lunch_combo omni_Infinix-X6515-eng
+add_lunch_combo twrp_Infinix-X6515-user
+add_lunch_combo twrp_Infinix-X6515-userdebug
+add_lunch_combo twrp_Infinix-X6515-eng
