274179bb41e78a579839633e9797339e86d9c0ee
r4dbz <108780431+r4db@users.noreply.github.com>
2025-05-30 02:12:26 +0800

    Update device.mk

--- device.mk
+++ device.mk
@@ -5,7 +5,7 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-LOCAL_PATH := device/infinix/Infinix-X6515
+LOCAL_PATH := device/infinix/X6515
 # A/B
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \
