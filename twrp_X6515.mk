6015214f6f32fdbf2e1d11081a44c14b5195e3d2
r4dbz <108780431+r4db@users.noreply.github.com>
2025-05-30 02:10:56 +0800

    Update and rename twrp_Infinix-X6515.mk to twrp_X6515.mk

--- twrp_X6515.mk
+++ twrp_X6515.mk
@@ -13,10 +13,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from Infinix-X6515 device
-$(call inherit-product, device/infinix/Infinix-X6515/device.mk)
+$(call inherit-product, device/infinix/X6515/device.mk)
 
 PRODUCT_DEVICE := Infinix-X6515
-PRODUCT_NAME := twrp_Infinix-X6515
+PRODUCT_NAME := twrp_X6515
 PRODUCT_BRAND := Infinix
 PRODUCT_MODEL := Infinix X6515
 PRODUCT_MANUFACTURER := infinix
